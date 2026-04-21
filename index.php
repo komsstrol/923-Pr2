b5fa259c1154fdc7a175a1e1ed7a7a3149b3f0d1
komsstrol <trolkoms483@gmail.com>
2026-04-21 11:58:05 +0600

    Задание 11

--- index.php
+++ index.php
@@ -5,24 +5,40 @@
     <title>Программирование на языке PHP</title>
 </head>
 <body>
-
+    
     <h1>Основы программирования</h1>
     <h2>Типы данных</h2>
     <hr>
     <h2>Слабая типизация</h2>
     
     <?php
-        echo '1) 11 + \'7\' = ' . (11 + '7') . '<br>';
-        echo '2) \'2\' + \'5\' = ' . ('2' + '5') . '<br>';
-        echo '3) 2 + \'7 abc\' = ' . (2 + '7 abc') . '<br>';
-        echo '4) 2 + \'abc7\' = ' . (2 + 'abc7') . '<br>';
-        echo '5) 1 + true + null = ' . (1 + true + null) . '<br>';
-        echo '6) 1 + (int) \'abc7\' = ' . (1 + (int) 'abc7') . '<br>';
-        echo '7) 2 * \'7abc\' = ' . (2 * '7abc') . '<br>';
-        echo '8) true + false + true + true = ' . (true + false + true + true) . '<br>';
-        echo '9) \'8\' / \'2\' - \'null\' = ' . ('8' / '2' - 'null') . '<br>';
-        echo '10) \'4.5px\' - 3 = ' . ('4.5px' - 3) . '<br>';
+        $VarStr = 'Слабая типизация PHP';
+        
+        const CONSTSTR = 'Слабая типизация PHP';
+        
+        define("ARRSTR", array('Слабая типизация PHP'));
+        
+        echo "<b>Переменная \$VarStr:</b><br>";
+        var_dump($VarStr);
+        echo "<br><br>";
+        
+        echo "<b>Константа CONSTSTR:</b><br>";
+        var_dump(CONSTSTR);
+        echo "<br><br>";
+        
+        echo "<b>Массив ARRSTR:</b><br>";
+        var_dump(ARRSTR);
+        echo "<br><br>";
+        
+        echo "<hr>";
+        echo "<p><b>Вывод:</b> Тип данных зависит от способа представления данных в программе.</p>";
+        echo "<p>Значение 'Слабая типизация PHP' может быть: </p>";
+        echo "<ul>";
+        echo "<li>строкой (в переменной и константе)</li>";
+        echo "<li>массивом (в константе-массиве)</li>";
+        echo "</ul>";
+        echo "<p>var_dump() подтверждает разные типы для разных способов представления.</p>";
     ?>
-
+    
 </body>
 </html>
